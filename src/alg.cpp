3cb74a7ed675612bd9b9b5030c8fd7e6b0e1700b
belovtut <artem.belov2004@yandex.ru>
2025-02-23 22:32:16 +0300

    add 4 functions for prime numbers lib; add 14 unit tests

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,24 +1,44 @@
-// Copyright 2025 UNN-CS
 #include <cstdint>
 #include "alg.h"
 
 
 bool checkPrime(uint64_t value) {
-  // вставьте код функции
-  return true;
+	if (value <= 1) return false;
+	if (value == 2 || value == 3) return true;
+	if (value % 2 == 0 || value % 3 == 0) return false;
+
+	for (uint64_t i = 5; i * i <= value; i += 6) {
+		if (value % i == 0 || value % (i + 2) == 0) { return false; }
+	}
+	return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-  // вставьте код функции
-  return 2;
+	if (n == 0) return 0; // zeroth prime number not defined
+	uint64_t count = 0;
+	uint64_t candidate = 2; // '2' - first prime number
+
+	while (count < n) {
+		if (checkPrime(candidate)) { count++; }
+		candidate++;
+	}
+	return candidate - 1;
 }
 
 uint64_t nextPrime(uint64_t value) {
-  // вставьте код функции
-  return 2;
+	uint64_t candidate = value + 1;
+
+	while (true) {
+		if (checkPrime(candidate)) { return candidate; }
+		candidate++;
+	}
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-  // вставьте код функции
-  return 2;
+	uint64_t sum = 0;
+
+	for (uint64_t i = 2; i < hbound; i++) {
+		if (checkPrime(i)) { sum += i; }
+	}
+	return sum;
 }
